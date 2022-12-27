f83b32ad322a53f74d4fe9ed47e1874e42d69dd5
barakatokikiola <barakattajudeen2018@gmail.com>
2022-12-27 20:22:39 +0100

    updated

--- src/Calc.js
+++ src/Calc.js
@@ -1,75 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Calc extends React.Component {
-  constructor(props){
-    super(props);
-    this.state ={
-      height: null, 
-      weight: null
-    };
-  }
+const Calc =(props) => {
+    const [height, setHeight] = useState(null);
+    const [weight, setWeight] = useState(null);
+    const [result, setResult] = useState('');
+    const [bmiRange, setRange] = useState('');
+
   //Takes the user height and weight
-    userInputHandler=(e) => {
-      let nam = e.target.name;
-      let val = e.target.value;
-      this.setState({[nam]:val});
-      
+    const handleHeight=(e)=>{
+      setHeight(e.target.value)
+    }
+    const handleWeight=(e)=>{
+      setWeight(e.target.value)
     }
     
+    const handleReset =() => {
+      setHeight(null);
+      setWeight(null);
+      setResult('');
+      setRange('');
+    }
     //Calculates user BMI and Shows the BMI range
-    bmiCalc=(event)=>{
-      let height = this.state.height
-      let weight = this.state.weight
+    function bmiCalc(){
       let bmi = weight/ (height/100 * height/100);
       let bmiOutput= bmi.toFixed(2);
-        let bmiO = `Your BMI is ${bmiOutput}
-      kg/m²`;
-    document.getElementById('result').textContent = bmiO;
-      
+       setResult(`Your BMI is ${bmiOutput}
+       kg/m²`)
+  
       let a = "BMI Range : ";
  
       if(bmiOutput < 18.5){
-      document.getElementById('bmiRange').textContent = a + "Underweight";
+          setRange( a + "Underweight");
       } else if (bmiOutput === 18.5 || bmiOutput < 25 ) {
-          document.getElementById('bmiRange').textContent = a + "Your BMI is in the normal range";
+          setRange(a + "Your BMI is in the normal range");
       }
        else if (bmiOutput === 25 || bmiOutput < 30) {
-         document.getElementById('bmiRange').textContent = a + "Overweight";
+         setRange( a + "Overweight");
        }
         else if ( bmiOutput > 30) {
-          document.getElementById('bmiRange').textContent = a + "Obese";
+          setRange(a + "Obese");
         }
     }
+   
     
-    //Resets users input
-   resetHandler=()=>{
-    document.getElementById('height').value = '';
-    document.getElementById('weight').value = '';
-    document.getElementById('bmiRange').textContent = '';
-     document.getElementById('result').textContent = '';
-   }
-    render() {
        return (
          <div id ='container'>
          <form>
             <h1>BMI CALCULATOR</h1>
           <h4> Height</h4>
-           <input type='text' name="height" class= 'inputed' id = 'height' onChange={this.userInputHandler}
+           <input type='text' className='inputed' name="height"  onChange={handleHeight}
            />
          <h4>  Weight </h4>
-           <input type='text' name ='weight' class= 'inputed' id='weight' onChange={this.userInputHandler}
+           <input type='text' className='inputed' name ='weight' onChange={handleWeight}
              />
-           <input type = 'button' onClick = {this.bmiCalc} id= "button" value = 'Get BMI'/>
-            <input type = 'button' id = 'button' onClick={this.resetHandler}value = 'Reset'/>
-                <p id = 'result'></p>
-                <h4 id='bmiRange'></h4>
+           <input type = 'button' onClick={bmiCalc} id= "button" value = 'Get BMI'/>
+            <input type = 'reset' id = 'button' value = 'Reset' onClick={handleReset}/>
+                <p>{result}</p>
+                <h4>{bmiRange}</h4>
                 <p id = 'info'> Please enter your height(cm) and your weight(kg)</p>
             </form>
            
             </div>
        );
     };
-}
+
    
   
 
